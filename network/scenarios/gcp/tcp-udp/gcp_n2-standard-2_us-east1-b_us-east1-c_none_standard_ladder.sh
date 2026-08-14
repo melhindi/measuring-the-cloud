aa00a2f3829903bacbb756de3d2095b4b32182d6
melhindi <mu7eh7@gmail.com>
2026-08-14 13:22:24 +0200

    Move the GCP intra/inter arms to us-east1, which has capacity

--- network/scenarios/gcp/tcp-udp/gcp_n2-standard-2_us-east1-b_us-east1-c_none_standard_ladder.sh
+++ network/scenarios/gcp/tcp-udp/gcp_n2-standard-2_us-east1-b_us-east1-c_none_standard_ladder.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Inter-zone: us-east4-c to us-east4-a, one region.
+# Inter-zone: us-east1-b to us-east1-c, one region.
 #
 # The first inter-zone GCP scenario in the repository -- the existing GCP
 # scenarios cover intra-zone and cross-region only, which left a hole exactly
@@ -10,13 +10,13 @@
 # two zones, and instances in different zones are already on different hosts.
 source "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)/common.inc"
 
-SCENARIO_NAME=gcp_n2-standard-2_us-east4-c_us-east4-a_none_standard_ladder
+SCENARIO_NAME=gcp_n2-standard-2_us-east1-b_us-east1-c_none_standard_ladder
 OS_TUNING=standard
 INSTANCE_AFFINITY=none
 CLIENT_MACHINE_TYPE=n2-standard-2
 SERVER_MACHINE_TYPE=n2-standard-2
-CLIENT_REGION=us-east4
-CLIENT_AVAILABILITY_ZONE=us-east4-c
-SERVER_AVAILABILITY_ZONE=us-east4-a
+CLIENT_REGION=us-east1
+CLIENT_AVAILABILITY_ZONE=us-east1-b
+SERVER_AVAILABILITY_ZONE=us-east1-c
 PLACEMENT_MODE=multi-az
 CPU_IDLE_PINNING=1
